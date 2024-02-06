5bc7756466f2fbbd6b6d92bb1a35be37b58eadcd
Zhi Xi Tee <2802089t@student.gla.ac.uk>
2024-02-06 01:12:13 +0000

    .

--- rango/models.py
+++ rango/models.py
@@ -3,6 +3,8 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
+    NAME_MAX_LENGTH = 128
+
     name = models.CharField(max_length=128, unique=True)
     views = models.IntegerField(default=0)
     likes = models.IntegerField(default=0)
@@ -19,6 +21,9 @@ class Category(models.Model):
         return self.name
     
 class Page(models.Model):
+    TITLE_MAX_LENGTH = 128
+    URL_MAX_LENGTH = 200
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(max_length=128)
     url = models.URLField()
