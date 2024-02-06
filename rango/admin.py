d30193f772e30a4bd2861abee0bb9e9712da3200
Zhi Xi Tee <2802089t@student.gla.ac.uk>
2024-02-06 00:34:42 +0000

    .

--- rango/admin.py
+++ rango/admin.py
@@ -5,5 +5,8 @@ from rango.models import Category, Page
 class PageAdmin(admin.ModelAdmin):
     list_display = ('title', 'category', 'url')
 
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slugs':('name',)}
+
 admin.site.register(Category)
 admin.site.register(Page, PageAdmin)
